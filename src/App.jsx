b0ab89bf22e210c984b682c29bb6e04cdfcf41f0
stephanobenny03 <stephanobenny03gmail.com>
2026-07-15 11:06:06 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
+    {/* <BrowserRouter>
+    
+    <Routes>
+
+      <Route path='/' element={}/>
+      <Route path='/add' element={}/>
+      <Route path='/view' element={}/>
+      <Route path='/addinstall' element={}/>
+      <Route path='/viewintall' element={}/>
+      <Route path='/addteam' element={}/>
+      <Route path='/viewteam' element={}/>
+      <Route path='/addpayment' element={}/>
+      <Route path='/viewpayment' element={}/>
+
+
+
+    </Routes>
+    
+    </BrowserRouter> */}
+    <NavBar/>
     </>
   )
 }
